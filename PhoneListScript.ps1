a2a058bdd8b886d8db203bf7e87f3a27a47e9039
JonasBSaxild <43033098+Fr0thgar@users.noreply.github.com>
2025-02-17 15:51:01 +0100

    Update PhoneListScript.ps1
    fucking hell

--- PhoneListScript.ps1
+++ PhoneListScript.ps1
@@ -1,25 +1,38 @@
 ﻿# Check current modules
-Get-Module -ListAvailable
+#Get-Module -ListAvailable
 
 # Update PnP PowerShell module
-Update-Module -Name PnP.PowerShell
+#Update-Module -Name PnP.PowerShell
 
 # If issues persist, uninstall and reinstall
 #Uninstall-Module -Name PnP.PowerShell
 #Install-Module -Name PnP.PowerShell
 
 # Check PowerShell version
-$PSVersionTable.PSVersion
+#$PSVersionTable.PSVersion
 
 # Import the PnP Powershell module
 Import-Module PnP.PowerShell
 
+# Variables for app registration
+$siteUrl = "https://twcas.sharepoint.com/HR"  # Ensure this is correct
+$clientId = "d704eda0-54af-4fad-a62a-546401569152"
+$tenantId = "38898116-fa71-4dd1-b72d-09c5fd8a7141"
+
+
 # Connect to Sharepoint
-$siteUrl = "https://twcas.sharepoint.com/HR/Lists/Telefonliste/Allitemsg.aspx"
-Connect-PnPOnline -Url $siteUrl -Interactive
+Connect-PnPOnline -Url $siteUrl -ClientId $clientId -Tenant $tenantId -Interactive
+
+# Test the connection
+#try {
+ #   $web = Get-PnPWeb
+  #  Write-Host "Connected to SharePoint site: $(web.Title)"
+#} catch {
+ #   Write-Host "Failed to connect to SharePoint: $_"    
+#}
 
 # Path to the CSV File
-$csvPath = "\\filsrv\it\Phonelist\upload_template.csv"
+$csvPath = "\\filsrv\it\Telefonliste\Telefonliste.csv"
 
 # Read the CSV File
 $phoneList = Import-Csv -Path $csvPath
@@ -28,7 +41,7 @@ $phoneList = Import-Csv -Path $csvPath
 foreach($row in $phoneList) {
     try {
         #Check if the item already exists in the list
-        $existingItem = Get-PnPListItem -List "PhoneList" -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>$($row.Name)</Value></Eq></Where></Query></View>"
+        $existingItem = Get-PnPListItem -List "Telefonliste" -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>$($row.Name)</Value></Eq></Where></Query></View>"
     } catch {
         Write-Host "Error retrieving item: $_"
         Get-PnPException
@@ -38,7 +51,7 @@ foreach($row in $phoneList) {
     if ($existingItem){
         try{
             # Update the existing item
-            Set-PnPListItem -List "PhoneLIst" -Identity $existingItem.Id -Values @{
+            Set-PnPListItem -List "Telefonliste" -Identity $existingItem.Id -Values @{
                 "Direkte"   = $row.Direkte
                 "Mobil"     = $row.Mobil
                 "Mail"      = $row.Mail
@@ -55,7 +68,7 @@ foreach($row in $phoneList) {
     } else {
         try {
             #  Add a new item
-            Add-PnPListItem -List "PhoneList" -Values @{
+            Add-PnPListItem -List "Telefonliste" -Values @{
                 "Title"     = $row.Name
                 "Direkte"   = $row.Direkte
                 "Mobil"     = $row.Mobil
